4efc47f8cf6ce6b3ddf6717219698a06c23382e9
Harsh Dubey <harshdu001@gmail.com>
2026-06-28 20:00:42 +0530

    Move Redis credentials to environment variables

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -3,11 +3,11 @@ const { createClient } =require('redis');
 
 
 const redisClient = createClient({
-    username: 'default',
-    password: '2ryxrb4MqG0lBz9lz5UClg3q9hFvMbDP',
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-10434.c267.us-east-1-4.ec2.cloud.redislabs.com',
-        port: 10434
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT)
     }
 });
 
