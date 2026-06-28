a7a30cc77a1ed5e0f568ec77f170f486e3e90bbc
Harsh Dubey <harshdu001@gmail.com>
2026-06-28 19:00:53 +0530

    Unnecessary Comments Removed

--- Frontend/src/Components/problems/CodeEditor.jsx
+++ Frontend/src/Components/problems/CodeEditor.jsx
@@ -1,20 +1,3 @@
-// import Editor from "@monaco-editor/react";
-
-// const CodeEditor = ({ code, setCode, language }) => {
-
-//   return (
-//     <Editor
-//       height="500px"
-//       language={language}
-//       theme="vs-dark"
-//       value={code}
-//       onChange={(value) => setCode(value)}
-//     />
-//   );
-// };
-
-// export default CodeEditor;
-
 import Editor from "@monaco-editor/react";
 
 const CodeEditor = ({ code, setCode, language }) => {
